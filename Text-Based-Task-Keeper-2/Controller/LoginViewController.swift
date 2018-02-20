b9d81ceb9169cacdc326c10afb11e0d96111b594
Miki Nomura <miki@fyi.tokyo>
2018-02-19 19:57:37 -0800

    Add SignUp / SignIn Screen

--- Text-Based-Task-Keeper-2/Controller/LoginViewController.swift
+++ Text-Based-Task-Keeper-2/Controller/LoginViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,12 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func signInButtonTapped(_ sender: UIButton) {
     
+    }
+    
+    @IBAction func signUpButtonTapped(_ sender: UIButton) {
+        
+    }
 }
 
