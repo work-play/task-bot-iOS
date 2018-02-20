db2f35e61ca6ac9dbf2c18892dd472a83b909a9d
Miki Nomura <miki@fyi.tokyo>
2018-02-19 18:54:57 -0800

    Initial Commit

---
+++ Text-Based-Task-Keeper-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Text-Based-Task-Keeper-2
+//
+//  Created by Miki Nomura on 2/19/18.
+//  Copyright © 2018 Miki Nomura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
